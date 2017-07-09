7fbb9f3453f33ebbeec5e469e494f051e0257127
Lilit Yenokyan <lilityenokyan1997@gmail.com>
2017-07-09 21:36:47 +0400

    Deleted primes.html, added primes.php

---
+++ primes.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lilit
+ * Date: 7/9/17
+ * Time: 8:44 PM
+ */
+
+function checkPrime() {
+    for ($i = 2; $i < 100; $i++) {
+        $primeTrue = true;
+        for ($j = 2; $j <= sqrt($i); $j++) {
+            if ($i % $j === 0) {
+                $primeTrue = false;
+            }
+        }
+        if ($primeTrue) {
+            echo $i;
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Primes</title>
+
+</head>
+<body>
+
+
+    <p id="line">Prime numbers less than 100: </p>
+
+
+
+</body>
+</html>
