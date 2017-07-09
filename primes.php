efa7d6f26643de1793d99731c0c8404ebdb2e21c
lility <lilityenokyan1997@gmail.com>
2017-07-09 23:10:18 +0400

    Update primes.php
    Fixed mistakes:both syntax and logic

--- primes.php
+++ primes.php
@@ -1,3 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <meta charset="UTF-8">
+        <title>Primes</title>
+
+    </head>
+    <body>
+
+        <p id="line">Prime numbers less than 100: </p>
+
+    </body>
+</html>
+
 <?php
 /**
  * Created by PhpStorm.
@@ -5,36 +21,17 @@
  * Date: 7/9/17
  * Time: 8:44 PM
  */
-
 function checkPrime() {
     for ($i = 2; $i < 100; $i++) {
         $primeTrue = true;
         for ($j = 2; $j <= sqrt($i); $j++) {
             if ($i % $j === 0) {
                 $primeTrue = false;
+                break;
             }
         }
-        if ($primeTrue) {
-            echo $i;
-        }
+            echo $i.' ';
     }
 }
+checkPrime();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-    <meta charset="UTF-8">
-    <title>Primes</title>
-
-</head>
-<body>
-
-
-    <p id="line">Prime numbers less than 100: </p>
-
-
-
-</body>
-</html>
